f786700efa38389d06d4428319185b776a052c16
Hector Valdivia <hect.valdivia@gmail.com>
2021-05-17 02:00:42 -0600

    Tailwind config add folder for purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
     purge: [
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php'
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
     ],
 
     theme: {
