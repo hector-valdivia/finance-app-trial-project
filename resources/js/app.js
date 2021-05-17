4d0fef0dd44a4ecaed25c6f0612a40db28088652
Hector Valdivia <hect.valdivia@gmail.com>
2021-05-17 02:02:27 -0600

    Configuration for inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
 require('./bootstrap');
+
+import {App, plugin} from '@inertiajs/inertia-vue'
+import Vue from 'vue'
+import store from './store'
+
+Vue.use(plugin)
+
+Vue.filter('datetime', function (value, format = 'D MMM, YYYY hh:mm a') {
+    return value? $dayjs.utc(value).format(format) : ' - ';
+});
+
+const el = document.getElementById('app')
+
+new Vue({
+    store,
+    render: h => h(App, {
+        props: {
+            initialPage: JSON.parse(el.dataset.page),
+            resolveComponent: name => require(`./Pages/${name}`).default,
+        },
+    }),
+}).$mount(el)
